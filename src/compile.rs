af30ac34f0680515ba67151c1d781d6b50c702d6
memoryleak47 <memoryleak47@protonmail.ch>
2022-09-30 01:08:00 +0200

    add struct Compile

--- src/compile.rs
+++ src/compile.rs
@@ -6,76 +6,102 @@ use llvm::prelude::*;
 
 const EMPTY: *const i8 = b"\0".as_ptr() as *const _;
 
-// prints the corresponding module to stdout
 pub fn compile(ast: &Ast) {
-    unsafe {
-        // Set up a context, module and builder in that context.
-        let context = LLVMContextCreate();
-        let module = LLVMModuleCreateWithNameInContext(b"luamod\0".as_ptr() as *const _, context);
-        let builder = LLVMCreateBuilderInContext(context);
+    Compile::new()
+        .compile(ast)
+}
 
-        let i32t = LLVMInt32TypeInContext(context);
-        let voidt = LLVMVoidType();
-        let zero = LLVMConstInt(i32t, 0, 0);
+struct Compile {
+    context: LLVMContextRef,
+    module: LLVMModuleRef,
+    builder: LLVMBuilderRef,
+    i32t: LLVMTypeRef,
+    voidt: LLVMTypeRef,
+    vars: HashMap<String, LLVMValueRef>,
+}
 
-        // declare print
-        let mut argts_print = [i32t];
-        let print_type = LLVMFunctionType(voidt, argts_print.as_mut_ptr(), 1, 0);
-        let print_function = LLVMAddFunction(module, b"extra_print\0".as_ptr() as *const _, print_type);
+impl Compile {
+    fn new() -> Compile {
+        unsafe {
+            let context = LLVMContextCreate();
+            let module = LLVMModuleCreateWithNameInContext(b"luamod\0".as_ptr() as *const _, context);
+            let builder = LLVMCreateBuilderInContext(context);
 
-        // create main
-        let main_function_type = LLVMFunctionType(i32t, [].as_mut_ptr(), 0, 0);
-        let main_function = LLVMAddFunction(module, b"main\0".as_ptr() as *const _, main_function_type);
+            let i32t = LLVMInt32TypeInContext(context);
+            let voidt = LLVMVoidType();
+            let vars = HashMap::new();
 
-        let bb = LLVMAppendBasicBlockInContext(context, main_function, b"entry\0".as_ptr() as *const _);
-        LLVMPositionBuilderAtEnd(builder, bb);
-
-        let mut vars: HashMap<String, LLVMValueRef> = HashMap::new();
-
-        for st in &ast.statements {
-            match st {
-                Statement::Print(expr) => {
-                    let mut args = [compile_expr(context, builder, expr, &mut vars)];
-                    LLVMBuildCall2(
-                        /*builder: */ builder,
-                        /*type: */ print_type,
-                        /*Fn: */ print_function,
-                        /*Args: */ args.as_mut_ptr(),
-                        /*Num Args: */ args.len() as u32,
-                        /*Name: */ EMPTY,
-                    );
-                },
-                _ => println!("not yet done! ignoring.."),
+            Compile {
+                context,
+                module,
+                builder,
+                i32t,
+                voidt,
+                vars,
             }
         }
+    }
 
-        LLVMBuildRet(builder, zero);
+    fn compile(&mut self, ast: &Ast) {
+        unsafe {
+            let mut argts_print = [self.i32t];
+            let print_type = LLVMFunctionType(self.voidt, argts_print.as_mut_ptr(), 1, 0);
+            let print_function = LLVMAddFunction(self.module, b"extra_print\0".as_ptr() as *const _, print_type);
 
-        // done building
-        LLVMDisposeBuilder(builder);
+            // create main
+            let main_function_type = LLVMFunctionType(self.i32t, [].as_mut_ptr(), 0, 0);
+            let main_function = LLVMAddFunction(self.module, b"main\0".as_ptr() as *const _, main_function_type);
 
-        // Dump the module as IR to stdout.
-        LLVMDumpModule(module);
+            let bb = LLVMAppendBasicBlockInContext(self.context, main_function, b"entry\0".as_ptr() as *const _);
+            LLVMPositionBuilderAtEnd(self.builder, bb);
 
-        LLVMContextDispose(context);
+            for st in &ast.statements {
+                match st {
+                    Statement::Print(expr) => {
+                        let mut args = [self.compile_expr(expr)];
+                        LLVMBuildCall2(
+                            /*builder: */ self.builder,
+                            /*type: */ print_type,
+                            /*Fn: */ print_function,
+                            /*Args: */ args.as_mut_ptr(),
+                            /*Num Args: */ args.len() as u32,
+                            /*Name: */ EMPTY,
+                        );
+                    },
+                    _ => println!("not yet done! ignoring.."),
+                }
+            }
+
+            let zero = LLVMConstInt(self.i32t, 0, 0);
+            LLVMBuildRet(self.builder, zero);
+        }
+    }
+
+    fn compile_expr(&mut self, expr: &Expr) -> LLVMValueRef {
+        unsafe {
+            match expr {
+                Expr::Var(var) => {
+                    self.vars[var]
+                },
+                Expr::LiteralNum(x) => {
+                    LLVMConstInt(self.i32t, *x as u64, 0)
+                },
+                Expr::Plus(l, r) => {
+                    let l = self.compile_expr(l);
+                    let r = self.compile_expr(r);
+                    LLVMBuildAdd(self.builder, l, r, EMPTY)
+                },
+            }
+        }
     }
 }
 
-fn compile_expr(context: LLVMContextRef, builder: LLVMBuilderRef, expr: &Expr, vars: &mut HashMap<String, LLVMValueRef>) -> LLVMValueRef {
-    unsafe {
-        match expr {
-            Expr::Var(var) => {
-                vars[var]
-            },
-            Expr::LiteralNum(x) => {
-                let i32t = LLVMInt32TypeInContext(context);
-                LLVMConstInt(i32t, *x as u64, 0)
-            },
-            Expr::Plus(l, r) => {
-                let l = compile_expr(context, builder, l, vars);
-                let r = compile_expr(context, builder, r, vars);
-                LLVMBuildAdd(builder, l, r, EMPTY)
-            },
+impl Drop for Compile {
+    fn drop(&mut self) {
+        unsafe {
+            LLVMDisposeBuilder(self.builder);
+            LLVMDumpModule(self.module);
+            LLVMContextDispose(self.context);
         }
     }
 }
