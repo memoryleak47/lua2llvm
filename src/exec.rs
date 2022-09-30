0d22609ca6bf2e067c2ad687d9abd435e6532e6e
memoryleak47 <memoryleak47@protonmail.ch>
2022-09-30 05:08:05 +0200

    <No commit-message>

--- src/exec.rs
+++ src/exec.rs
@@ -3,45 +3,76 @@ use crate::*;
 use std::collections::HashMap;
 
 #[derive(Default)]
-struct Exec {
+struct StackFrame {
     vars: HashMap<String, u32>,
-    fns: HashMap<String, Vec<Statement>>,
+    statements: Vec<Statement>,
+    next_statement: usize,
+}
+
+#[derive(Clone)]
+struct Function {
+    args: Vec<String>,
+    statements: Vec<Statement>,
+}
+
+#[derive(Default)]
+struct Exec {
+    stack: Vec<StackFrame>,
+    fns: HashMap<String, Function>,
 }
 
 pub fn exec(ast: &Ast) {
-    Exec::new().exec(ast)
+    let mut e = Exec::default();
+    e.stack.push(StackFrame {
+        vars: Default::default(),
+        statements: ast.statements.clone(),
+        next_statement: 0,
+    });
+    e.exec();
 }
 
 impl Exec {
-    fn new() -> Exec {
-        Default::default()
-    }
-
-    fn exec(&mut self, ast: &Ast) {
-        for st in &ast.statements {
-            self.exec_statement(st);
+    fn exec(&mut self) {
+        while let Some(f) = self.stack.last_mut() {
+            if f.next_statement < f.statements.len() {
+                let stmt = f.statements[f.next_statement].clone();
+                f.next_statement += 1;
+                self.exec_statement(&stmt);
+            } else {
+                self.stack.pop();
+            }
         }
     }
 
     fn exec_statement(&mut self, stmt: &Statement) {
         match stmt {
             Statement::FunctionCall { fn_name, args } => {
+                let args: Vec<u32> = args.iter().map(|expr| self.eval_expr(expr) ).collect();
                 if fn_name == "print" {
                     for arg in args {
-                        println!("{}", self.eval_expr(arg));
+                        println!("{}", arg);
                     }
                 } else {
-                    let statements = self.fns[fn_name].clone();
-                    for st in statements {
-                        self.exec_statement(&st);
+                    let function = self.fns[fn_name].clone();
+                    let mut sf = StackFrame::default();
+                    sf.statements = function.statements.clone();
+                    for (name, value) in function.args.iter().zip(args.iter()) {
+                        sf.vars.insert(name.clone(), *value);
                     }
+                    self.stack.push(sf);
                 }
             },
             Statement::Assign { var, expr } => {
-                self.vars.insert(var.clone(), self.eval_expr(expr));
+                let val = self.eval_expr(expr);
+                let frame: &mut StackFrame = match self.stack.iter_mut().rfind(|f| f.vars.contains_key(var)) {
+                    Some(x) => x,
+                    None => &mut self.stack[0],
+                };
+                frame.vars.insert(var.clone(), val);
             }
             Statement::FunctionDef { fn_name, args, body } => {
-                self.fns.insert(fn_name.clone(), body.clone());
+                let function = Function { args: args.clone(), statements: body.clone() };
+                self.fns.insert(fn_name.clone(), function);
             }
         }
     }
@@ -49,7 +80,10 @@ impl Exec {
     fn eval_expr(&self, expr: &Expr) -> u32 {
         match expr {
             Expr::LiteralNum(x) => *x,
-            Expr::Var(var) => *self.vars.get(var).expect("variable not defined!"),
+            Expr::Var(var) => {
+                let frame: &StackFrame = self.stack.iter().rfind(|f| f.vars.contains_key(var)).expect("variable not defined!");
+                *frame.vars.get(var).unwrap()
+            }
             Expr::Plus(a, b) => self.eval_expr(a) + self.eval_expr(b),
         }
     }
