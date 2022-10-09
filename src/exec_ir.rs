53ecf306d42561bb73dc2038c85ecbad1940d57f
memoryleak47 <memoryleak47@protonmail.ch>
2022-10-09 21:03:09 +0200

    <No commit-message>

--- src/exec_ir.rs
+++ src/exec_ir.rs
@@ -8,8 +8,29 @@ type NativeFnId = usize;
 static NATIVE_FNS: [NativeFn; 1] = [print_fn];
 
 fn print_fn(t: TablePtr, ctxt: &mut Ctxt) -> TablePtr {
-    todo!()
+    match table_get(t, Value::Num(1.0), ctxt) {
+        Value::Nil => println!("nil"),
+        Value::Bool(b) => println!("{}", b),
+        Value::Str(s) => println!("{}", s),
+        Value::TablePtr(ptr) => println!("table {}", ptr),
+        Value::NativeFn(_) => println!("<native-fn>"),
+        Value::LuaFn(..) => println!("<lua-fn>"),
+        Value::Num(x) => println!("{}", x),
+    }
+
+    // return empty table
+    let ptr = ctxt.heap.len();
+    ctxt.heap.push(TableData::default());
+    ptr
 }
+
+fn table_get(ptr: TablePtr, idx: Value, ctxt: &mut Ctxt) -> Value {
+    ctxt.heap[ptr].entries.iter()
+        .find(|(x, _)| *x == idx)
+        .map(|(_, v)| v.clone())
+        .unwrap_or(Value::Nil)
+}
+
 
 #[derive(Clone, PartialEq, Debug)]
 enum Value {
