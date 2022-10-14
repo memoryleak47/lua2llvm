6774cd6d35f8dbc50b017fc1c3669f2ad3514973
memoryleak47 <memoryleak47@protonmail.ch>
2022-10-14 12:42:29 +0200

    add failing closure test

---
+++ tests/37.lua
@@ -0,0 +1,14 @@
+local fff = 12
+foo = function()
+    local f = function()
+        return function()
+           fff = fff-1
+        end
+    end
+    (f())()
+    return f()
+end
+
+
+(foo())()
+print(fff)
