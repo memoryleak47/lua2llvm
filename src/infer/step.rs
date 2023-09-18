962c822c243ebe891a24e070e9493d79cd0103e0
memoryleak47 <memoryleak47@protonmail.ch>
2023-09-18 20:48:50 +0200

    infer: impl constants

--- src/infer/step.rs
+++ src/infer/step.rs
@@ -1,16 +1,18 @@
 use crate::infer::*;
 
 pub(in crate::infer) fn infer_step(st: &Statement, (fid, bid, sid): Stmt, inf: &mut Infer, ir: &IR) {
-    let nxt_stmt = || (fid, bid, sid+1);
+    let mut state: LocalState = inf.fn_state[&fid].state[&(bid, sid)].clone();
     match st {
-        Statement::Compute(n, expr) => infer_step_compute(*n, expr, (fid, bid, sid), inf),
+        Statement::Compute(n, expr) => {
+            infer_step_compute(*n, expr, &mut state, inf);
+            to_stmt((fid, bid, sid+1), state, inf);
+        },
         Statement::Store(t, i, v) => {
-            let mut state: LocalState = inf.fn_state[&fid].state[&(bid, sid)].clone();
             let t: Value = state.nodes[&t].clone();
             let i: Value = state.nodes[&i].clone();
             let v: Value = state.nodes[&v].clone();
             state.class_states.set(&t, &i, &v);
-            to_stmt(nxt_stmt(), state, inf);
+            to_stmt((fid, bid, sid+1), state, inf);
         },
 
         Statement::If(cond, then, else_) => {
@@ -25,7 +27,7 @@ pub(in crate::infer) fn infer_step(st: &Statement, (fid, bid, sid): Stmt, inf: &
             match cmd {
                 Command::Print(_) => {
                     let current_state = inf.fn_state[&fid].state[&(bid, sid)].clone();
-                    to_stmt(nxt_stmt(), current_state, inf);
+                    to_stmt((fid, bid, sid+1), current_state, inf);
                 }
                 Command::Throw(_) => {
                     // nothing to do after this, nothing gets "dirty".
@@ -39,7 +41,8 @@ pub(in crate::infer) fn infer_step(st: &Statement, (fid, bid, sid): Stmt, inf: &
     }
 }
 
-fn infer_step_compute(n: Node, expr: &Expr, (fid, bid, sid): Stmt, inf: &mut Infer) {
+fn infer_step_compute(n: Node, expr: &Expr, state: &mut LocalState, inf: &mut Infer) {
+    let mut v = Value::bot();
     match expr {
         Expr::Index(t, i) => unimplemented!(),
         Expr::Arg => unimplemented!(),
@@ -50,27 +53,28 @@ fn infer_step_compute(n: Node, expr: &Expr, (fid, bid, sid): Stmt, inf: &mut Inf
         Expr::Intrinsic(i) => unimplemented!(),
 
         Expr::Num(num) => {
-            let mut state: LocalState = inf.fn_state[&fid].state[&(bid, sid)].clone();
-            let mut v = Value::bot();
             let num = (*num).try_into().unwrap();
             v.nums = Lattice::Set(vec![num].into_iter().collect());
-
-            state.nodes.insert(n, v);
-            to_stmt((fid, bid, sid+1), state, inf);
         },
-        Expr::Bool(b) => unimplemented!(),
-        Expr::Nil => unimplemented!(),
-        Expr::Str(s) => unimplemented!(),
+        Expr::Bool(b) => {
+            v.bools = vec![*b].into_iter().collect();
+        },
+        Expr::Nil => {
+            v.nils = vec![()].into_iter().collect();
+        },
+        Expr::Str(s) => {
+            v.strings = Lattice::Set(vec![s.to_string()].into_iter().collect());
+        },
     }
+    state.nodes.insert(n, v);
 }
 
-fn to_stmt(new: Stmt, state: LocalState, inf: &mut Infer) {
-    let (fid, bid, sid) = new;
+fn to_stmt((fid, bid, sid): Stmt, state: LocalState, inf: &mut Infer) {
     let def = LocalState::default();
     let old_state = inf.fn_state[&fid].state.get(&(bid, sid)).unwrap_or(&def);
     let result_state = state.merge(old_state);
     if &result_state != old_state {
         inf.fn_state.get_mut(&fid).unwrap().state.insert((bid, sid), result_state);
-        inf.dirty.push(new);
+        inf.dirty.push((fid, bid, sid));
     }
 }
