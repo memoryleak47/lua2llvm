cdd24e3818f9be2a8c722c0e1af10ab880297364
memoryleak47 <memoryleak47@protonmail.ch>
2023-10-30 00:08:46 +0100

    use ordered_map_iter to print fns and blocks

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -6,6 +6,7 @@ mod infer;
 use crate::ir::*;
 use crate::infer::*;
 
+use std::collections::HashMap;
 use std::fmt::{self, Formatter};
 
 const INLINE_CONST_NODES: bool = false;
@@ -20,10 +21,16 @@ pub fn ir_to_string(ir: &IR, inf: Option<&Infer>) -> String {
 }
 
 fn fmt_ir(ir: &IR, inf: Option<&Infer>, f: &mut Formatter<'_>) -> fmt::Result {
-    for (&fid, _) in &ir.fns {
+    for (&fid, _) in ordered_map_iter(&ir.fns) {
         let mut fobj = FnDisplayObj::new(ir, inf);
         fobj.display_fn(fid, f)?;
     }
 
     Ok(())
+}
+
+fn ordered_map_iter<K: Ord, V>(map: &HashMap<K, V>) -> impl Iterator<Item=(&K, &V)> + '_ {
+    let mut v: Vec<_> = map.iter().collect();
+    v.sort_by_key(|x| x.0);
+    v.into_iter()
 }
