4877bee03cf0f36e05c4a0993f1472d9c5a5b9ca
memoryleak47 <memoryleak47@protonmail.ch>
2023-12-04 00:35:21 +0100

    describe struct-layout plan

--- src/layout.rs
+++ src/layout.rs
@@ -15,6 +15,10 @@ pub enum TableLayout {
     Struct(Vec<Value>),
 }
 
+// a Location `l` is Struct-layoutable, if
+// - All its set & get usages have completely inferred concrete non-table keys (bools/nums/strings).
+// - There exists no `Value` in the infer that contains *l/@l and additionally any other table. (Neither a Node-Value, nor a Value in the ClassStates)
+// - Next is never applied on @l|*l.
 pub fn layout(ir: &IR, inf: &Infer) -> Layout {
     // TODO
     Layout { table_layouts: HashMap::new() }
